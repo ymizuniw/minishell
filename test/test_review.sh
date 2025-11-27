15f5533d7a16e0d66c06ddd284b49a9e20a85e05
Yugo Mizuniwa <ymizuniw@Yugos-MacBook-Air.local>
2025-11-28 01:52:04 +0900

    concise test script just forreview modified

--- test/test_review.sh
+++ test/test_review.sh
@@ -1,94 +1,95 @@
 #!/bin/bash
 
-echo "=============test for reveiew===================="
+echo "============= test for review ===================="
 
-echo "=============simple command======================"
+echo "============= simple command ====================="
 
-echo "=========Abs Path=========="
+echo "========= Abs Path =========="
 /bin/ls
 /bin/echo
 /bin/touch in
 /bin/cat in
-/bin/echo "infile" >>in
+/bin/echo "infile" >> in
 /bin/cat in
 /bin/rm -f in
 
-echo "=========Rel Path=========="
+echo "========= Rel Path =========="
 ls
 echo
 touch in
-cat in
-echo "infile" >> in
+echo "infile" > in
 cat in
 rm -f in
 
-echo "==========cd abs/rel/~==============="
-cd .. && pwd cd test && pwd
-tmp_path=$PWD
-cd $HOME && pwd
-cd tmp_path && pwd
+echo "========== cd abs / rel / ~ ==============="
+cd .. && pwd
+cd test && pwd
+TMP_PATH="$PWD"
+cd "$HOME" && pwd
+cd "$TMP_PATH" && pwd
 cd ~ && pwd
-cd tmp_path && pwd
+cd "$TMP_PATH" && pwd
 
-echo "=============with several argments====================="
+echo "============= several arguments ====================="
 echo a a
-cat a a
+cat a a || echo "cat failed"
 ls a a
-touch in && echo "infile">in
-touch in2 && echo "infile2">in2
+touch in && echo "infile" > in
+touch in2 && echo "infile2" > in2
 cat in in2
-ls src includes
+ls src includes || echo "ls failed"
 rm -f in in2
 
-echo "=============pipe====================="
+echo "============= pipe ====================="
 echo a a | cat || echo "cat failed"
 cat a a | cat || echo "cat failed"
 ls a a | cat
-touch in && echo "infile">in
-touch in2 && echo "infile2">in2
+touch in && echo "infile" > in
+touch in2 && echo "infile2" > in2
 cat in in2 | cat
 rm -f in in2
 
-echo "============Environment Variable================"
+echo "============ Environment Variable ================"
 echo $USER $? $HOME
-echo "$USER" '$?' "$H"OME
-echo $USER$?$HOME
+echo "$USER" '$?' "$HOME"
+echo ${USER}${?}${HOME}
 echo "'$USER' $HOME"
-echo '"$USER" $HOME'
+echo "\"$USER\" $HOME"
 
-
-echo "============Unset Path==============="
-INPUT="
-unset $PATH
+echo "============ Unset Path ==============="
+INPUT=$(cat << 'EOF'
+unset PATH
 echo $PATH
 touch in
 /bin/touch in && echo "infile" > in
 cat in || echo "cat failed"
 /bin/cat in || echo "cat failed"
 exit
-"
+EOF
+)
+
 cd ..
-echo $INPUT | ./minishell
+echo "$INPUT" | ./minishell
 cd test
 
-echo "============Set Path================="
-mkdir test1 && mkdir test2
-touch /test1/test1.sh && touch /test2/test1.sh
-TEST1_FILE="/test1/test1.sh"
-TEST2_FILE="/test2/test1.sh"
-chmod +x $TEST1_FILE
-chmod +x $TEST2_FILE
-echo "#!/bin/bash" >> $TEST1_FILE
-echo "#!/bin/bash" >> $TEST2_FILE
-echo "
-">> $TEST1_FILE
-echo "
-">> $TEST2_FILE
-echo "echo test1's test">>$TEST1_FILE
-echo "echo test2's test">>$TEST2_FILE
-TEST1_PATH="/$PWD/$TEST1_FILE"
-TEST2_PATH="/$PWD/$TEST2_FILE"
+echo "============ Set Path ==============="
+
+mkdir -p test1
+mkdir -p test2
+
+echo "#!/bin/bash" > test1/test1.sh
+echo "echo test1's test" >> test1/test1.sh
+chmod +x test1/test1.sh
+
+echo "#!/bin/bash" > test2/test2.sh
+echo "echo test2's test" >> test2/test2.sh
+chmod +x test2/test2.sh
+
+TEST1_PATH="$PWD/test1"
+TEST2_PATH="$PWD/test2"
+
 export PATH="$TEST1_PATH:$TEST2_PATH:$PATH"
-./test1
+test1.sh
+
 export PATH="$TEST2_PATH:$TEST1_PATH:$PATH"
-./test2
+test2.sh
