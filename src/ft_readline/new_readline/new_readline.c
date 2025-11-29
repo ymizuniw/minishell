be419be29f0d58d156d5d87cd85e8122b1996907
Yugo Mizuniwa <ymizuniw@Yugos-MacBook-Air.local>
2025-11-29 14:16:45 +0900

    new_readline update:

--- src/ft_readline/new_readline/new_readline.c
+++ src/ft_readline/new_readline/new_readline.c
@@ -4,33 +4,78 @@
 
 typedef struct s_new_readline
 {
-    const char *prompt;
-    size_t      prompt_len;
+	const char		*prompt;
+	size_t			prompt_len;
 
-    char        buf[1024];//please init.
-    size_t      buf_len;
-    size_t      cursor_buf_pos;
+	char			buf[1024]; // please init
+	size_t			buf_len;
+	size_t			cursor_buf_pos;
 
-    t_hist      *hist;
+	t_hist			*hist;
 
-    size_t      cursor_x;
-    size_t      cursor_y;
-    size_t      terminal_width;
-    size_t      terminal_height;
-    struct termios raw_mode;
-    struct termios original;
-}t_new_readline;
+	size_t			cursor_x;
+	size_t			cursor_y;
+	size_t			terminal_width;
+	size_t			terminal_height;
+	struct termios	raw_mode;
+	struct termios	original;
+}	t_new_readline;
 
-//window resize signal_handler()
-//to SIGWINCH
-void handle_sigwinch(int signum)
+
+void	add_history(char *line, t_hist *hist)
 {
-    g_signum = signum;
+	if (!line || !*line || hist == NULL)
+		return ;
+	if (hist->hist_box[hist->idx] != NULL)
+		xfree(hist->hist_box[hist->idx]);
+	hist->hist_box[hist->idx] = ft_strdup(line);
+	hist->idx = (hist->idx + 1) % HIST_MAX;
+	if (hist->size < HIST_MAX)
+		hist->size++;
+	hist->cur = -1;
 }
 
-int ft_putchar(int c)
+void	free_hist_box(char *hist_box[HIST_MAX])
 {
-    return (write(STDOUT_FILENO, &c, 1));
+	size_t	i;
+
+	i = 0;
+	while (i < HIST_MAX)
+	{
+		xfree(hist_box[i]);
+		hist_box[i] = NULL;
+		i++;
+	}
+}
+
+
+void	enable_raw_mode(struct termios *orig)
+{
+	struct termios	raw;
+
+	tcgetattr(STDIN_FILENO, orig);
+	raw = *orig;
+	raw.c_lflag &= ~(ICANON | ECHO | ISIG);
+	raw.c_cc[VMIN] = 1;
+	raw.c_cc[VTIME] = 0;
+	tcsetattr(STDIN_FILENO, TCSANOW, &raw);
+}
+
+void	disable_raw_mode(const struct termios *orig)
+{
+	tcsetattr(STDIN_FILENO, TCSANOW, orig);
+}
+
+
+void	handle_sigwinch(int signum)
+{
+	g_signum = signum;
+}
+
+
+int	ft_putchar(int c)
+{
+	return (write(STDOUT_FILENO, &c, 1));
 }
 
 #define CLEAR_LINE "\r\x1b[K"
@@ -40,135 +85,98 @@ int ft_putchar(int c)
 #define ANSI_RIGHT "\x1b[C"
 #define ANSI_LEFT "\x1b[D"
 
-//if explicit term_buffer is provided.
-// char *buffer = (char *)malloc(ft_strlen(term_buffer));
-// #define BUFFADDR &buffer
+static void	copy_history_to_buffer(t_new_readline *nr);
 
-// cl_string = tgetstr("cl", BUFFADDR);
-// cm_string = tgetstr("cm", BUFFADDR);
-// auto_wrap = tgetstr("am");
-// height = tgetnum("li");
-// width = tgetnum("co");
-
-//padding char for waiting tputs();
-// temp = tgetstr("pc", BUFFADDR);
-// PC = temp ? *temp : 0;
-// BC = tgetstr("le", BUFFADDR);
-// UP = tgetstr("up", BUFFADDR);
-
-// int redraw_line(t_new_readline *nr)
-// {
-//     size_t total = nr->prompt_len + nr->buf_len;
-//     size_t rows = total / nr->terminal_width + 1;
-
-//     // (1) 行頭
-//     write(STDOUT_FILENO, "\r", 1);
-
-//     // (2) 既存行を全消去
-//     for (size_t r = 0; r < rows; r++)
-//     {
-//         write(STDOUT_FILENO, CLEAR_LINE, CLEAR_LINE_LEN);
-//         write(STDOUT_FILENO, "\n", 1);
-//     }
-
-//     // (3) 最終行の行頭に戻る
-//     write(STDOUT_FILENO, "\r", 1);
-
-//     // (4) 再描画
-//     write(STDOUT_FILENO, nr->prompt, nr->prompt_len);
-//     write(STDOUT_FILENO, nr->buf, nr->buf_len);
-
-//     // (5) カーソル位置へ移動
-//     size_t pos = nr->prompt_len + nr->cursor_buf_pos;
-//     size_t row = pos / nr->terminal_width;
-//     size_t col = pos % nr->terminal_width;
-
-//     char *cm = tgetstr("cm", NULL);
-//     char *goto_str = tgoto(cm, col, row);
-//     tputs(goto_str, 1, ft_putchar);
-
-//     return 1;
-// }
-
-
-int redraw_line(t_new_readline *nr)
+static int	redraw_line(t_new_readline *nr)
 {
-    write(STDOUT_FILENO, CLEAR_LINE, (size_t)CLEAR_LINE_LEN);
-    write(STDOUT_FILENO,nr->prompt, nr->prompt_len);
-    write(STDOUT_FILENO, nr->buf, nr->buf_len);
+	size_t	offset;
+	size_t	current;
 
-    size_t offset = nr->prompt_len + nr->cursor_buf_pos;
-    size_t current = nr->prompt_len + nr->buf_len;
-    while (current > offset)
-    {
-        write(STDOUT_FILENO, ANSI_LEFT, sizeof(ANSI_LEFT)-1);
-        current--;
-    }
-    //cursor_x = prompt_len + 1;?
-    return(1);
+	write(STDOUT_FILENO, CLEAR_LINE, (size_t)CLEAR_LINE_LEN);
+	write(STDOUT_FILENO, nr->prompt, nr->prompt_len);
+	write(STDOUT_FILENO, nr->buf, nr->buf_len);
+	offset = nr->prompt_len + nr->cursor_buf_pos;
+	current = nr->prompt_len + nr->buf_len;
+	while (current > offset)
+	{
+		write(STDOUT_FILENO, ANSI_LEFT, sizeof(ANSI_LEFT) - 1);
+		current--;
+	}
+	return (1);
 }
 
-bool read_char(char *one_char)
+static bool	read_char(char *one_char)
 {
-    ssize_t rb = 0;
+	ssize_t	rb;
 
-    rb = read(STDIN_FILENO,one_char,1);
-    if (rb<0)
-    {
-        perror("read");
-        return (false);
-    }
-    return (true);
+	rb = read(STDIN_FILENO, one_char, 1);
+	if (rb < 0)
+	{
+		perror("read");
+		return (false);
+	}
+	return (true);
 }
 
-bool is_signal(char one_char)
+static char	*read_non_interactive(void)
 {
-    if (one_char == ASC_ETX || one_char == ASC_EOT)
-        return (true);
-    return (false);
+	char	*buf;
+	size_t	len;
+
+	buf = get_next_line(STDIN_FILENO);
+	if (!buf)
+		return (NULL);
+	len = ft_strlen(buf);
+	if (len > 0 && buf[len - 1] == '\n')
+		buf[len - 1] = '\0';
+	return (buf);
 }
 
-int process_signal(t_new_readline *nr, char c)
+static bool	is_signal_char(char c)
 {
-    if (c == ASC_ETX)
-    {
-        nr->buf_len = 0;
-        nr->buf[0] = '\0';
-        nr->cursor_buf_pos = 0;
-        write(STDOUT_FILENO, "^C\n", 3);
-        write(STDOUT_FILENO, nr->prompt, nr->prompt_len);
-        return (2);
-    }
-    if (c == ASC_EOT)
-        return (0);
-    return -1;
+	return (c == ASC_ETX || c == ASC_EOT);
 }
 
-bool is_del(char one_char)
+static int	process_signal(t_new_readline *nr, char c)
 {
-    if (one_char=='\b' || one_char == 0x7f)
-        return (true);
-    return (false);
+	if (c == ASC_ETX)
+	{
+		nr->buf_len = 0;
+		nr->buf[0] = '\0';
+		nr->cursor_buf_pos = 0;
+		write(STDOUT_FILENO, "^C\n", 3);
+		write(STDOUT_FILENO, nr->prompt, nr->prompt_len);
+		return (2);
+	}
+	if (c == ASC_EOT)
+		return (0);
+	return (-1);
 }
 
-int process_del(t_new_readline *nr, char one_char)
+static bool	is_del(char one_char)
 {
-    if (nr->buf_len==0 || nr->cursor_buf_pos==0)
-        return(0);
-    size_t pos = nr->cursor_buf_pos - 1;
-    ft_memmove(&nr->buf[pos], &nr->buf[pos + 1], nr->buf_len - pos -1);
-    nr->buf_len--;
-    nr->buf[nr->buf_len]='\0';
-    nr->cursor_buf_pos--;
-    redraw_line(nr);
-    return (1);
+	return (one_char == '\b' || one_char == 0x7f);
 }
 
-bool is_esc(char one_char)
+static int	process_del(t_new_readline *nr, char one_char)
 {
-    if (one_char==0x1b)
-        return (true);
-    return (false);
+	size_t	pos;
+
+	(void)one_char;
+	if (nr->buf_len == 0 || nr->cursor_buf_pos == 0)
+		return (0);
+	pos = nr->cursor_buf_pos - 1;
+	ft_memmove(&nr->buf[pos], &nr->buf[pos + 1], nr->buf_len - pos - 1);
+	nr->buf_len--;
+	nr->buf[nr->buf_len] = '\0';
+	nr->cursor_buf_pos--;
+	redraw_line(nr);
+	return (1);
+}
+
+static bool	is_esc(char one_char)
+{
+	return (one_char == 0x1b);
 }
 
 #define CSI_UP   "[A"
@@ -176,214 +184,227 @@ bool is_esc(char one_char)
 #define CSI_RIGHT "[C"
 #define CSI_LEFT "[D"
 
-int process_esc(t_new_readline *nr, char one_char)
+static int	process_up(t_new_readline *nr)
 {
-    char esq_buf[3]={0};
-    read_char(&esq_buf[0]);
-    //read fail handling. [
-    read_char(&esq_buf[1]);
-    //read fail handling. A, B.
-    if (ft_strcmp(esq_buf, CSI_UP)==0)
-        process_up(nr);//hist up.
-    else if (ft_strcmp(esq_buf, CSI_DOWN)==0)
-        process_down(nr);//hist down.
-    else if (ft_strcmp(esq_buf, CSI_RIGHT)==0)
-        process_right(nr);
-    else if (ft_strcmp(esq_buf, CSI_LEFT)==0)
-        process_left(nr);
-    return (1);
+	if (!nr->hist || nr->hist->size == 0)
+		return (0);
+	if (nr->hist->cur == -1)
+		nr->hist->cur = (nr->hist->idx + HIST_MAX - 1) % HIST_MAX;
+	else
+		nr->hist->cur = (nr->hist->cur + HIST_MAX - 1) % HIST_MAX;
+	if (nr->hist->hist_box[nr->hist->cur] == NULL)
+		return (0);
+	copy_history_to_buffer(nr);
+	redraw_line(nr);
+	return (1);
 }
 
-int process_up(t_new_readline *nr)
+static int	process_down(t_new_readline *nr)
 {
-    if (!nr->hist || nr->hist->size==0)
-        return (0);
-    if (nr->hist->cur == -1)
-        nr->hist->cur = (nr->hist->idx + HIST_MAX - 1) % HIST_MAX;
-    else
-        nr->hist->cur = (nr->hist->cur + HIST_MAX - 1) % HIST_MAX;
-    if (nr->hist->hist_box[nr->hist->cur]==NULL)
-        return (0);
-    copy_history_to_buffer(nr);
-    redraw_line(nr);
-    return (1);
+	if (!nr->hist || nr->hist->size == 0 || nr->hist->cur == -1)
+		return (0);
+	nr->hist->cur = (nr->hist->cur + 1) % HIST_MAX;
+	if (nr->hist->cur == nr->hist->idx)
+	{
+		nr->buf[0] = '\0';
+		nr->buf_len = 0;
+		nr->cursor_buf_pos = 0;
+		nr->hist->cur = -1;
+	}
+	else
+		copy_history_to_buffer(nr);
+	redraw_line(nr);
+	return (1);
 }
 
-int process_down(t_new_readline *nr)
+static int	process_right(t_new_readline *nr)
 {
-    if (!nr->hist || nr->hist->size == 0 || nr->hist->cur == -1)
-        return 0;
-    nr->hist->cur = (nr->hist->cur + 1) % HIST_MAX;
-    if (nr->hist->cur == nr->hist->idx)
-    {
-        nr->buf[0] = '\0';
-        nr->buf_len = 0;
-        nr->cursor_buf_pos = 0;
-        nr->hist->cur = -1;
-    }
-    else
-    {
-        copy_history_to_buffer(nr);
-    }
-    redraw_line(nr);
-    return 1;
+	if (nr->cursor_buf_pos < nr->buf_len)
+	{
+		nr->cursor_buf_pos++;
+		nr->cursor_x++;
+		write(1, ANSI_RIGHT, sizeof(ANSI_RIGHT) - 1);
+	}
+	return (1);
 }
 
-int process_right(t_new_readline *nr)
+static int	process_left(t_new_readline *nr)
 {
-    if (nr->cursor_buf_pos < nr->buf_len)
-    {
-        nr->cursor_buf_pos++;
-        nr->cursor_x++;
-        write(1, ANSI_RIGHT, sizeof(ANSI_RIGHT) - 1);
-    }
-    return (1);
+	if (nr->cursor_buf_pos > 0)
+	{
+		nr->cursor_buf_pos--;
+		nr->cursor_x--;
+		write(1, ANSI_LEFT, sizeof(ANSI_LEFT) - 1);
+	}
+	return (1);
 }
 
-int process_left(t_new_readline *nr)
+static int	process_esc(t_new_readline *nr, char one_char)
 {
-    if (nr->cursor_buf_pos > 0)
-    {
-        nr->cursor_buf_pos--;
-        nr->cursor_x--;
-        write(1, ANSI_LEFT, sizeof(ANSI_LEFT)-1);
-    }
-    return (1);
+	char	esq_buf[3];
+
+	(void)one_char;
+	ft_memset(esq_buf, 0, sizeof(esq_buf));
+	if (!read_char(&esq_buf[0]))
+		return (1);
+	if (!read_char(&esq_buf[1]))
+		return (1);
+	if (ft_strcmp(esq_buf, CSI_UP) == 0)
+		process_up(nr);
+	else if (ft_strcmp(esq_buf, CSI_DOWN) == 0)
+		process_down(nr);
+	else if (ft_strcmp(esq_buf, CSI_RIGHT) == 0)
+		process_right(nr);
+	else if (ft_strcmp(esq_buf, CSI_LEFT) == 0)
+		process_left(nr);
+	return (1);
 }
 
-bool is_inserting(t_new_readline *nr)
+static bool	is_inserting(t_new_readline *nr)
 {
-    if (nr->cursor_buf_pos < nr->buf_len)
-        return (true);
-    return (false);
+	return (nr->cursor_buf_pos < nr->buf_len);
 }
 
-int insertion(t_new_readline *nr, char one_char)
+static int	insertion(t_new_readline *nr, char one_char)
 {
-    size_t second_len;
-    if (nr->buf_len==1023)//termination place kept?
-        return (0);
-    second_len = nr->buf_len - nr->cursor_buf_pos;
-    ft_memmove(&nr->buf[nr->cursor_buf_pos + 1], &nr->buf[nr->cursor_buf_pos], second_len);
-    nr->buf[nr->cursor_buf_pos] = one_char;
-    nr->buf_len++;
-    nr->buf[nr->buf_len] = '\0';
-    nr->cursor_buf_pos++;
-    nr->cursor_x++;
-    return (1);
+	size_t	second_len;
+
+	if (nr->buf_len >= 1023)
+		return (0);
+	second_len = nr->buf_len - nr->cursor_buf_pos;
+	ft_memmove(&nr->buf[nr->cursor_buf_pos + 1],
+		&nr->buf[nr->cursor_buf_pos], second_len);
+	nr->buf[nr->cursor_buf_pos] = one_char;
+	nr->buf_len++;
+	nr->buf[nr->buf_len] = '\0';
+	nr->cursor_buf_pos++;
+	nr->cursor_x++;
+	return (1);
 }
 
-int write_to_buffer(t_new_readline *nr, char one_char)
+static int	write_to_buffer(t_new_readline *nr, char one_char)
 {
-    if (nr->buf_len>=1023)
-        return (0);
-    nr->buf[nr->buf_len] = one_char;
-    nr->buf_len++;
-    nr->cursor_buf_pos = nr->buf_len;
-    nr->buf[nr->buf_len] = '\0';
-    nr->cursor_x++;
-    return (1);
+	if (nr->buf_len >= 1023)
+		return (0);
+	nr->buf[nr->buf_len] = one_char;
+	nr->buf_len++;
+	nr->cursor_buf_pos = nr->buf_len;
+	nr->buf[nr->buf_len] = '\0';
+	nr->cursor_x++;
+	return (1);
 }
 
-int process_printable(t_new_readline *nr, char one_char)
+static int	process_printable(t_new_readline *nr, char one_char)
 {
-    //nr->cursor_x
-    //nr->cursor_y
-    int ret;
-    if (is_inserting(nr))
-        ret = insertion(nr, one_char);
-    else
-        ret = write_to_buffer(nr, one_char);
-    redraw_line(nr);
-    return (ret);
+	int	ret;
+
+	if (is_inserting(nr))
+		ret = insertion(nr, one_char);
+	else
+		ret = write_to_buffer(nr, one_char);
+	redraw_line(nr);
+	return (ret);
 }
 
-int resize_window(t_new_readline *nr)
+static int	resize_window(t_new_readline *nr)
 {
-    struct winsize w;
+	struct winsize	w;
 
-    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w)>=0)
-    {
-        nr->terminal_width = w.ws_col;
-        nr->terminal_height = w.ws_row;
-    }
-    else
-    {
-        perror("ioctl");
-        return (-1);
-    }
-    return (1);
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) >= 0)
+	{
+		nr->terminal_width = w.ws_col;
+		nr->terminal_height = w.ws_row;
+	}
+	else
+	{
+		perror("ioctl");
+		return (-1);
+	}
+	return (1);
 }
 
-char *new_readline(t_shell *shell)
+static void	copy_history_to_buffer(t_new_readline *nr)
 {
-    t_new_readline nr;
-    char one_char = 0;
-    char *line = NULL;
-    bool read_char_res = false;
+	size_t	hist_len;
+	char	*src;
 
-    ft_memset(&nr, 0, sizeof(t_new_readline));
-    ft_memset(&nr.buf, 0, sizeof(nr.buf));
-    nr.hist = shell->hist;
+	if (!nr->hist || nr->hist->cur < 0)
+		return ;
+	src = nr->hist->hist_box[nr->hist->cur];
+	if (!src)
+		return ;
+	hist_len = ft_strlen(src);
+	if (hist_len > sizeof(nr->buf) - 1)
+		hist_len = sizeof(nr->buf) - 1;
+	ft_strlcpy(nr->buf, src, sizeof(nr->buf));
+	nr->buf_len = hist_len;
+	nr->cursor_buf_pos = hist_len;
+}
 
-    enable_raw_mode(&nr.original);
+char	*new_readline(t_shell *shell)
+{
+	t_new_readline	nr;
+	char			one_char;
+	char			*line;
+	bool			read_char_res;
 
-    nr.prompt = "minishell$";
-    nr.prompt_len = ft_strlen(nr.prompt);
-    write(STDOUT_FILENO, nr.prompt, nr.prompt_len);
+	if (!isatty(STDIN_FILENO) || !isatty(STDOUT_FILENO))
+		return (read_non_interactive());
+	ft_memset(&nr, 0, sizeof(t_new_readline));
+	ft_memset(&nr.buf, 0, sizeof(nr.buf));
+	nr.hist = shell->hist;
+	nr.prompt = "minishell$ ";
+	nr.prompt_len = ft_strlen(nr.prompt);
+	line = NULL;
+	one_char = 0;
+	read_char_res = false;
+	enable_raw_mode(&nr.original);
+	resize_window(&nr);
+	write(STDOUT_FILENO, nr.prompt, nr.prompt_len);
+	while (1)
+	{
+		if (g_signum == SIGWINCH)
+		{
+			resize_window(&nr);
+			redraw_line(&nr);
+			g_signum = 0;
+		}
+		read_char_res = read_char(&one_char);
+		if (!read_char_res)
+			break;
+		if (one_char == '\n')
+		{
+			write(STDOUT_FILENO, "\n", 1);
+			line = ft_strdup(nr.buf);
+			break;
+		}
+		else if (is_signal_char(one_char))
+		{
+			int	sig_res = process_signal(&nr, one_char);
 
-    while (1)
-    {
-        if (g_signum == SIGWINCH)
-        {
-            resize_window(&nr);
-            redraw_line(&nr);
-            g_signum = 0;
-        }
-        read_char_res = read_char(&one_char);
-        if (!read_char_res)
-            break;
-        if (one_char == '\n')
-        {
-            write(STDOUT_FILENO, "\n", 1);
-            line = ft_strdup(nr.buf);
-            break;
-        }
-        else if (is_signal(one_char))
-        {
-            int sig_res = process_signal(&nr, one_char);
-            if (sig_res == 0)
-            {
-                if (nr.buf_len == 0)
-                {
-                    free_rsc();
-                    line = NULL;
-                }
-                else if (sig_res == 2)
-                    continue ;
-                else
-                {
-                    line = ft_strdup(nr.buf);
-                    write(STDOUT_FILENO, "\n", 1);
-                }
-                break;
-            }
-            else if (sig_res == 1)
-            {
-                nr.buf_len = 0;
-                nr.buf[0] = '\0';
-                write(STDOUT_FILENO, "\n", 1);
-                line = ft_strdup("");
-                break;
-            }
-        }
-        else if (is_del(one_char))
-            process_del(&nr, one_char);
-        else if (is_esc(one_char))
-            process_esc(&nr, one_char);
-        else if (ft_isprint(one_char) || ft_isspace(one_char))
-            process_printable(&nr, one_char);
-    }
-    disable_raw_mode(&nr.original);
-    return line;
+			if (sig_res == 2)
+				continue ;
+			if (sig_res == 0)
+			{
+				if (nr.buf_len == 0)
+				{
+					free_rsc();
+					line = NULL;
+					break;
+				}
+				else
+					continue ;
+			}
+		}
+		else if (is_del(one_char))
+			process_del(&nr, one_char);
+		else if (is_esc(one_char))
+			process_esc(&nr, one_char);
+		else if (ft_isprint((int)one_char) || ft_isspace((int)one_char))
+			process_printable(&nr, one_char);
+	}
+	disable_raw_mode(&nr.original);
+	if (line && shell && shell->hist && nr.buf_len > 0)
+		add_history(line, shell->hist);
+	return (line);
 }
